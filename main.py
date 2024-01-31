79e931a1c521e210754c220b90fa5dfcaab62a04
ColeJ1115 <153942775+ColeJ1115@users.noreply.github.com>
2024-01-31 11:57:14 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,27 @@
-random_text = input('Enter some text! ')
-print(random_text)
-
-try:
-    random_number = int(input('Please enter a number. '))
-    print(random_number + 1)
-except ValueError:
-    print('That is not a number')
-
-random_float = float(input('Please enter a number with a decimal. '))
-print(random_float + .5)
-
-multiple_one = float(input('Please enter your first number to be multiplied. '))
-multiple_two = float(input('Please enter the second number to be multiplied. '))
-print(multiple_one * multiple_two)
-
-multiple_a = float(input('Please enter your first number to be divided. '))
-multiple_b = float(input('Please enter the second number to be divided. '))
-print(multiple_a / multiple_b)
+actual_temp = int(input('What is the temp of your home? '))
+desired_temp = int(input('What is the desired temp of your home? '))
 
 
-random_boo_value = (input('Enter a boolean value! (True or False) '))
-random_boo_value2 = random_boo_value.lower()
+def  heating_cooling(actual_temp, desired_temp):
+    if actual_temp < desired_temp:
+        print('Run Heat')
+    elif actual_temp > desired_temp:
+        print('Run A/C')
+    else:
+        print('Standby')
+    return
+temp = heating_cooling(actual_temp,desired_temp)
 
-if random_boo_value2 == 'true':
-    random_boo_value = True
-elif random_boo_value2 == 'false':
-    random_boo_value = False
-else:
-    random_boo_value != True or False
 
-if random_boo_value == True:
-    print("You entered True!")
-    print("The oppisite is false")
-elif random_boo_value == False:
-    print("You entered False!")
-    print("The oppisite is true!")
-else:
-    print('There is no value')
+def convert_temp(temp_c, target_U_input):
+    target_U_input = target_U_input.lower()
+    if target_U_input == 'k':
+        return (temp_c + 273.15)
+    elif target_U_input =='f':
+        return ((temp_c * 9/5) + 32)
+    elif target_U_input == 'c':
+        return (temp_c)
 
-# 1.7 alternate
 
-#Ask user for value
-user_input = input("Enter a boolean last time:")
-#right = True if literally typed True since the comparrison is correct
-#If not True typed then it is false
-right = user_input == "True"
-#This sets a boolean only value based off of if True matched what was typed
-right2 = bool(right)
-#Prints the results
-print("You entered:", right2)
-print("The opposite of what you entered is:", not right2)
+
+print(convert_temp(80, 'F'))
