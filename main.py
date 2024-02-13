e195b796b048a64d86414d31f888e30cd67c0348
ColeJ1115 <153942775+ColeJ1115@users.noreply.github.com>
2024-02-13 09:59:15 -0500

    Add files via upload
    Dictionary lab

--- main.py
+++ main.py
@@ -1,27 +1,56 @@
-actual_temp = int(input('What is the temp of your home? '))
-desired_temp = int(input('What is the desired temp of your home? '))
+def list_names(students):
+    index = 1
+    for student in students:
+        print(f"{index}. {student['name']}")
+        index += 1
 
+def get_new_student():
+    name = input("Please input a name for the new student:\n")
+    hometown = input("Next please input their hometown:\n")
+    favorite_food = input("Last please input their favorite food:\n")
+    return {"name": name, "hometown": hometown, "favorite_food": favorite_food}
 
-def  heating_cooling(actual_temp, desired_temp):
-    if actual_temp < desired_temp:
-        print('Run Heat')
-    elif actual_temp > desired_temp:
-        print('Run A/C')
-    else:
-        print('Standby')
-    return
-temp = heating_cooling(actual_temp,desired_temp)
+def main():
+    students = [
+        {'name': 'Cole Jenkins', 'hometown': 'Rock Hill SC', 'Favorite Food': 'Sushi'},
+        {'name': 'Sam Arewood', 'hometown': ' Mobile AL', 'Favorite Food': 'Fried Chicken'},
+        {'name': 'Fionna DaVinci', 'hometown': ' Los Angeles CA', 'Favorite Food': 'Pizza'}
+    ]
 
+    while True:
+        print("Please select which action you'd like to do: add, view, or quit")
+        action = input("> ").lower()
 
-def convert_temp(temp_c, target_U_input):
-    target_U_input = target_U_input.lower()
-    if target_U_input == 'k':
-        return (temp_c + 273.15)
-    elif target_U_input =='f':
-        return ((temp_c * 9/5) + 32)
-    elif target_U_input == 'c':
-        return (temp_c)
+        if action == 'add':
+            new_student = get_new_student()
+            students.append(new_student)
+            print("Student added successfully!")
+        elif action == 'view':
+            list_names(students)
+            while True:
+                choice = input("Which student would you like to learn more about? Enter a number 1-" + str(len(students)) + ":\n> ")
+                if choice.isdigit():
+                    index = int(choice) - 1
+                    if 0 <= index < len(students):
+                        student = students[index]
+                        print(f"Student {index+1} is {student['name']}. What would you like to know?")
+                        info = input("Enter 'hometown' or 'favorite food':\n> ").lower()
+                        if info == 'hometown':
+                            print(f"{student['name']} is from {student['hometown']}")
+                            break
+                        elif info == 'favorite food':
+                            print(f"{student['name']}'s favorite food is {student['favorite_food']}")
+                            break
+                        else:
+                            print("Invalid option! Please enter 'hometown' or 'favorite food'.")
+                    else:
+                        print("Invalid index! Please enter a number within the range.")
+                else:
+                    print("Invalid input! Please enter a valid number.")
+        elif action == 'quit':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid action! Please enter 'add', 'view', or 'quit'.")
 
-
-
-print(convert_temp(80, 'F'))
+main()
